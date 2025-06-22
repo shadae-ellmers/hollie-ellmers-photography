7795a7e2459ffe2ee1ce8acd91eb783e30241f5b
Shadae <shadaejadeellmers@gmail.com>
2025-06-22 18:30:10 +1200

    match navigation and other services arrow hover

--- components/OtherService.tsx
+++ components/OtherService.tsx
@@ -13,7 +13,7 @@ export default function OtherService() {
         className="group inline-flex items-center text-xl lg:text-2xl text-amber-50 transition-colors duration-300"
       >
         <span className="pr-4 group-hover:text-amber-50/80">Contact me</span>
-        <div className="transition-transform duration-300 group-hover:translate-x-1 fill-amber-50 h-full">
+        <div className="transition-transform duration-300 group-hover:translate-x-3 fill-amber-50 h-full">
           <ArrowRightIcon />
         </div>
       </a>
