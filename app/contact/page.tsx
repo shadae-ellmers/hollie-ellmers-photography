dbc49fd914a0756385eb33efa61c2548f0fd61a5
Shadae <shadaejadeellmers@gmail.com>
2025-10-27 17:29:59 +1300

    replace placeholder content on about, home, and contact pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -29,20 +29,6 @@ export default function Contact() {
         imageAlt="A bride and groom smile joyfully as confetti is thrown over them during an outdoor wedding ceremony, surrounded by happy guests. The bride holds a bouquet and the groom wears a light grey suit."
       />
       <style>{'.grecaptcha-badge { visibility: visible; }'}</style>
-      <div className="px-6 sm:px-12 py-8 sm:py-16 lg:text-lg lg:max-w-[1000px] w-full flex flex-col justify-centre mx-auto">
-        <p>
-          Here is some information on how to enquire, or how to contact me
-          outside this form.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
-          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-          consequat massa quis enim. Donec pede justo, fringilla vel, aliquet
-          nec, vulputate eget, arcu.
-        </p>
-      </div>
       <ContactForm />
     </div>
   )
