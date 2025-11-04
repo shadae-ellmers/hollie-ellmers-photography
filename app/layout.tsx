c49560949dff17bfdf08345547eeb375cd4a944e
Shadae <shadaejadeellmers@gmail.com>
2025-11-04 19:14:06 +1300

    add valid favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,10 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📸</text></svg>"
-        />
+        <link rel="icon" href="images/favicon-H.ico" />
       </head>
       <body className={`${poppins.variable} antialiased`}>
         <style>{'.grecaptcha-badge { visibility: hidden; }'}</style>
